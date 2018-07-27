8854a3fe5ee7d972575294a3e4b2aa0968a2f6a6
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-27 11:01:46 +0530

    Update hitha.java

--- hitha.java
+++ hitha.java
@@ -9,8 +9,8 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		for (int i = 0; i < 3; i++) {
-			System.out.println("Hello");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello\n");
 		}// your code goes here
 	}
 }
